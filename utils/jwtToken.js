36a200f55e3de1d09bbae3129ca3727a8886f9ab
Mehedi Hasan Raju <145052408+Mehedi-Hasan-Raju@users.noreply.github.com>
2026-07-07 00:11:37 +0600

    Enhance cookie security in token generation
    Added secure and sameSite attributes to cookie options.

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -3,9 +3,11 @@ export const generateToken = (user, message, statusCode, res) => {
     const token = user.generateJsonWebToken();
 
     res.status(statusCode).cookie("token", token, {
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-        httpOnly: true,
-    })
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: true,        
+    sameSite: "None",    // cross-site কুকি allow করার জন্য এটা দরকার
+})
     .json({
         success: true,
         message,
